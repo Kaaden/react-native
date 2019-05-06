9c64ee24954bfd057eb7d94b0ebde4df27dc8923
kaaden <794102675@qq.com>
2019-05-06 15:44:00 +0800

    update

--- src/page/home/index.js
+++ src/page/home/index.js
@@ -1,14 +1,13 @@
 import React from 'react';
 import { View, Text } from 'react-native';
 
-
 export default class Home extends React.Component {
     static navigationOptions = {
         title: '首页',
     };
     render() {
         return (
-            <View ><Text>12441</Text></View>
+            <View ><Text style={{ fontFamily: 'space-mono' }}>12441</Text></View>
         );
     }
 
