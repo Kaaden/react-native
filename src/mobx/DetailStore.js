06db006a1763a9478025e4cb9f5c09dfaa2a46cc
Kaaden <794102675@qq.com>
2019-05-17 14:36:20 +0800

    update

--- src/mobx/DetailStore.js
+++ src/mobx/DetailStore.js
@@ -19,10 +19,6 @@ class DetailStore {
     doData(data) {
         let payload = ""
         if (data) {
-            // const regex1 = new RegExp("(i?)(\<img)(?!(.*?style=['\"](.*)['\"])[^\>]+\>)", "gmi");
-            // data.content = data.content.replace(regex1, "$2 style=\"\"$3");
-            // const regex2 = new RegExp("(i?)(\<img.*?style=['\"])([^\>]+\>)", "gmi");
-            // data.content = data.content.replace(regex2, "$2display:block;height:300px;width:100%;$3");
             data.content = data.content.replace(/↵/g, "");
             payload = data
         }
--- src/page/Detail/index.js
+++ src/page/Detail/index.js
@@ -35,7 +35,6 @@ class Detail extends React.Component {
         this.state = ({
             webViewHeight: 0,
             webLoading: true,
-            height: 0
         })
     }
     componentDidMount() {
@@ -67,8 +66,7 @@ class Detail extends React.Component {
         <meta name="viewport" content="width=device-width,initial-scale=1.0, minimum-scale=1.0, maximum-scale=1.0, user-scalable=no"/>
     
         <body>
-        <div id="info"></div>
-        
+        ${content}
         <script>
         function ResizeImages(){
             var myimg;
@@ -79,97 +77,64 @@ class Detail extends React.Component {
             }
         }
         window.onload=function(){ 
-            document.getElementById("info").innerHTML = ${content};
-            window.location.hash = '#' + document.body.clientHeight;
-            document.title = document.body.clientHeight ;
+            ResizeImages()
         }
         </script></body></html>`
         return html
     }
-    onMessage (event) {
+    onMessage(event) {
+        console.log(event)
         try {
-          const action = JSON.parse(event.nativeEvent.data)
-          if (action.type === 'setHeight' && action.height > 0) {
-            this.setState({ height: action.height })
-          }
+            const action = JSON.parse(event.nativeEvent.data)
+            if (action.type === 'setHeight' && action.height > 0) {
+                this.setState({ webViewHeight: action.height })
+            }
         } catch (error) {
-          // pass
+            // pass
         }
-      }
+    }
     render() {
-        // const { webLoading, webViewHeight } = this.state
-        // const { data, loading } = this.store
+        const { webLoading, webViewHeight } = this.state
+        const { data, loading } = this.store
+      
         return (
-            <ScrollView>
-                <WebView
-                    injectedJavaScript={BaseScript}
-                    style={{
-                        width: Dimensions.get('window').width,
-                        height: this.state.height
-                    }}
-                    automaticallyAdjustContentInsets
-                    source={{ html: HTMLTEXT }}// 这里可以使用uri
-                    decelerationRate='normal'
-                    scalesPageToFit
-                    javaScriptEnabled // 仅限Android平台。iOS平台JavaScript是默认开启的。
-                    domStorageEnabled // 适用于安卓
-                    scrollEnabled={false}
-                    onMessage={this.onMessage.bind(this)}
+
+            <ScrollView style={styles.container}>
+                <StatusBar
+                    animated
+                    translucent
+                    barStyle={'dark-content'}
+                    networkActivityIndicatorVisible={true}
                 />
+                <Img propsImg={data.img} styles={styles.topImg} />
+                <SafeAreaView style={styles.bottom}>
+                    {this.LoadView(loading)}
+                    {!loading && <View style={styles.content}
+                        horizontal={false}>
+                        <View style={styles.bottomTop}>
+                            <Text style={styles.title}>{data.title}</Text>
+                            <View style={styles.more}>
+                                <Text style={styles.author}>{data.authors}</Text>
+                                <Text style={styles.time}>{data.time}</Text>
+                            </View>
+                        </View>
+
+                        <View style={{ paddingLeft: 20, marginTop: 20 }}>
+                            <View style={styles.desc}>
+                                <Text style={{ color: Color.desc }}>{data.description}</Text>
+                            </View>
+                        </View>
+                        <View style={{ paddingHorizontal: 20, width: Dimensions.get("window").width }}>
+                       
+                            <WebView
+                                style={{height:Dimensions.get("window").height}}
+                                source={{ uri: 'http://kaaden.orrzt.com/#/detail?id=1' }}
+                            />
+                        </View>
+
+                    </View>}
+                </SafeAreaView>
             </ScrollView>
-
-            // <ScrollView style={styles.container}>
-            //     <StatusBar
-            //         animated
-            //         translucent
-            //         barStyle={'dark-content'}
-            //         networkActivityIndicatorVisible={true}
-            //     />
-            //     <Img propsImg={data.img} styles={styles.topImg} />
-            //     <SafeAreaView style={styles.bottom}>
-            //         {this.LoadView(loading)}
-            //         {!loading && <View style={styles.content}
-            //             horizontal={false}>
-            //             <View style={styles.bottomTop}>
-            //                 <Text style={styles.title}>{data.title}</Text>
-            //                 <View style={styles.more}>
-            //                     <Text style={styles.author}>{data.authors}</Text>
-            //                     <Text style={styles.time}>{data.time}</Text>
-            //                 </View>
-            //             </View>
-
-            //             <View style={{ paddingLeft: 20, marginTop: 20 }}>
-            //                 <View style={styles.desc}>
-            //                     <Text style={{ color: Color.desc }}>{data.description}</Text>
-            //                 </View>
-            //             </View>
-            //             <View style={{ paddingHorizontal: 20, height: webViewHeight, width: Dimensions.get("window").width }}>
-            //                 {this.LoadView(webLoading)}
-            //                 <WebView
-            //                     ref={'webview'}
-            //                     source={{ html: this.getHtml(data.content) }}
-            //                     onLoadEnd={()=>this.setState({ webLoading: false })}
-            //                     style={{ flex: 1 }}
-            //                     bounces={true}
-            //                     scrollEnabled={false}
-            //                     javaScriptEnabled={true}
-            //                     automaticallyAdjustContentInsets={true}
-            //                     onNavigationStateChange={(title) => {
-            //                         console.log(title)
-            //                         if (title.title != undefined) {
-            //                             this.setState({
-            //                                 webViewHeight: 80000
-            //                             })
-            //                         }
-            //                     }}
-
-
-            //                 />
-            //             </View>
-
-            //         </View>}
-            //     </SafeAreaView>
-            // </ScrollView>
         );
     }
 
@@ -178,14 +143,12 @@ export default Detail
 const styles = StyleSheet.create({
     container: {
         width: "100%",
-        height: "100%",
     },
     top: {
         backgroundColor: "#FFFFFF",
     },
     bottom: {
         flex: 1,
-        height: "100%",
         backgroundColor: "#FFFFFF"
     },
     loading: {
@@ -232,7 +195,6 @@ const styles = StyleSheet.create({
     },
     content: {
         width: "100%",
-        height: "100%",
     },
 
 
