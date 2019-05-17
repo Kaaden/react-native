3a59e82cae202656ceeda86216f89aeb887156da
Kaaden <794102675@qq.com>
2019-05-17 13:42:08 +0800

    update

--- src/page/Detail/index.js
+++ src/page/Detail/index.js
@@ -2,6 +2,28 @@ import React from 'react';
 import { StyleSheet, StatusBar, SafeAreaView, View, Text, Dimensions, Alert, ScrollView, WebView } from 'react-native';
 import { observer, inject } from "mobx-react"
 import { Img, Color } from "../../components"
+
+const BaseScript =
+    `
+    (function () {
+        var height = null;
+        function changeHeight() {
+          if (document.body.scrollHeight != height) {
+            height = document.body.scrollHeight;
+            if (window.postMessage) {
+              window.postMessage(JSON.stringify({
+                type: 'setHeight',
+                height: height,
+              }))
+            }
+          }
+        }
+        setTimeout(changeHeight, 300);
+    } ())
+    `
+
+const HTMLTEXT = `<h1>This HTML snippet is now rendered with native components !</h1>
+    <img src="https://i.imgur.com/dHLmxfO.jpg?2" />`
 @inject("rootStore")
 @observer
 class Detail extends React.Component {
@@ -12,7 +34,8 @@ class Detail extends React.Component {
         this.store = DetailStore
         this.state = ({
             webViewHeight: 0,
-            webLoading: true
+            webLoading: true,
+            height: 0
         })
     }
     componentDidMount() {
@@ -44,7 +67,8 @@ class Detail extends React.Component {
         <meta name="viewport" content="width=device-width,initial-scale=1.0, minimum-scale=1.0, maximum-scale=1.0, user-scalable=no"/>
     
         <body>
-        ${content}
+        <div id="info"></div>
+        
         <script>
         function ResizeImages(){
             var myimg;
@@ -55,84 +79,97 @@ class Detail extends React.Component {
             }
         }
         window.onload=function(){ 
-            ResizeImages()
+            document.getElementById("info").innerHTML = ${content};
             window.location.hash = '#' + document.body.clientHeight;
             document.title = document.body.clientHeight ;
         }
         </script></body></html>`
         return html
     }
-    _webViewLoad = () => {
-        this.refs.webview.injectJavaScript(`
-        const height = document.body.scrollHeight;
-        window.postMessage(height);
-    `);
-        this.setState({ webLoading: false })
-    }
-    handleMessage(e) {
-        console.log(e)
-        this.setState({
-            WebViewHeight: e.nativeEvent.data
-        });
-    }
+    onMessage (event) {
+        try {
+          const action = JSON.parse(event.nativeEvent.data)
+          if (action.type === 'setHeight' && action.height > 0) {
+            this.setState({ height: action.height })
+          }
+        } catch (error) {
+          // pass
+        }
+      }
     render() {
-        const { webLoading, webViewHeight } = this.state
-        const { data, loading } = this.store
+        // const { webLoading, webViewHeight } = this.state
+        // const { data, loading } = this.store
         return (
-            <ScrollView style={styles.container}>
-                <StatusBar
-                    animated
-                    translucent
-                    barStyle={'dark-content'}
-                    networkActivityIndicatorVisible={true}
+            <ScrollView>
+                <WebView
+                    injectedJavaScript={BaseScript}
+                    style={{
+                        width: Dimensions.get('window').width,
+                        height: this.state.height
+                    }}
+                    automaticallyAdjustContentInsets
+                    source={{ html: HTMLTEXT }}// 这里可以使用uri
+                    decelerationRate='normal'
+                    scalesPageToFit
+                    javaScriptEnabled // 仅限Android平台。iOS平台JavaScript是默认开启的。
+                    domStorageEnabled // 适用于安卓
+                    scrollEnabled={false}
+                    onMessage={this.onMessage.bind(this)}
                 />
-                <Img propsImg={data.img} styles={styles.topImg} />
-                <SafeAreaView style={styles.bottom}>
-                    {this.LoadView(loading)}
-                    {!loading && <View style={styles.content}
-                        horizontal={false}>
-                        <View style={styles.bottomTop}>
-                            <Text style={styles.title}>{data.title}</Text>
-                            <View style={styles.more}>
-                                <Text style={styles.author}>{data.authors}</Text>
-                                <Text style={styles.time}>{data.time}</Text>
-                            </View>
-                        </View>
-
-                        <View style={{ paddingLeft: 20, marginTop: 20 }}>
-                            <View style={styles.desc}>
-                                <Text style={{ color: Color.desc }}>{data.description}</Text>
-                            </View>
-                        </View>
-                        <View style={{ paddingHorizontal: 20, height: webViewHeight, width: Dimensions.get("window").width }}>
-                            {this.LoadView(webLoading)}
-                            <WebView
-                                ref={'webview'}
-                                source={{ html: this.getHtml(data.content) }}
-                                onLoadEnd={this.webViewLoaded}
-                                style={{ flex: 1 }}
-                                bounces={true}
-                                scrollEnabled={false}
-                                javaScriptEnabled={true}
-                                automaticallyAdjustContentInsets={true}
-                                // onNavigationStateChange={(title) => {
-                                //     console.log(title)
-                                //     if (title.title != undefined) {
-                                //         this.setState({
-                                //             webViewHeight: (parseInt(title.title) + 20)
-                                //         })
-                                //     }
-                                // }}
-
-                                onMessage={(e) => {
-                                    this.handleMessage(e)
-                                }}
-                            />
-                        </View>
-
-                    </View>}
-                </SafeAreaView>
             </ScrollView>
+
+            // <ScrollView style={styles.container}>
+            //     <StatusBar
+            //         animated
+            //         translucent
+            //         barStyle={'dark-content'}
+            //         networkActivityIndicatorVisible={true}
+            //     />
+            //     <Img propsImg={data.img} styles={styles.topImg} />
+            //     <SafeAreaView style={styles.bottom}>
+            //         {this.LoadView(loading)}
+            //         {!loading && <View style={styles.content}
+            //             horizontal={false}>
+            //             <View style={styles.bottomTop}>
+            //                 <Text style={styles.title}>{data.title}</Text>
+            //                 <View style={styles.more}>
+            //                     <Text style={styles.author}>{data.authors}</Text>
+            //                     <Text style={styles.time}>{data.time}</Text>
+            //                 </View>
+            //             </View>
+
+            //             <View style={{ paddingLeft: 20, marginTop: 20 }}>
+            //                 <View style={styles.desc}>
+            //                     <Text style={{ color: Color.desc }}>{data.description}</Text>
+            //                 </View>
+            //             </View>
+            //             <View style={{ paddingHorizontal: 20, height: webViewHeight, width: Dimensions.get("window").width }}>
+            //                 {this.LoadView(webLoading)}
+            //                 <WebView
+            //                     ref={'webview'}
+            //                     source={{ html: this.getHtml(data.content) }}
+            //                     onLoadEnd={()=>this.setState({ webLoading: false })}
+            //                     style={{ flex: 1 }}
+            //                     bounces={true}
+            //                     scrollEnabled={false}
+            //                     javaScriptEnabled={true}
+            //                     automaticallyAdjustContentInsets={true}
+            //                     onNavigationStateChange={(title) => {
+            //                         console.log(title)
+            //                         if (title.title != undefined) {
+            //                             this.setState({
+            //                                 webViewHeight: 80000
+            //                             })
+            //                         }
+            //                     }}
+
+
+            //                 />
+            //             </View>
+
+            //         </View>}
+            //     </SafeAreaView>
+            // </ScrollView>
         );
     }
 
